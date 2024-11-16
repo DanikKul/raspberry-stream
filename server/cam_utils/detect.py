1988324ebe50ef772b4f13caf443bd89697ee279
Kulakovich Daniil <92942037+DanikKul@users.noreply.github.com>
2024-11-16 18:28:02 +0300

    fix detection

--- server/cam_utils/detect.py
+++ server/cam_utils/detect.py
@@ -4,8 +4,8 @@ import json
 
 def detect_cameras():
     valid_cams = []
-    for i in range(8):
-        cap = cv2.VideoCapture(i)
+    for i in range(1):
+        cap = cv2.VideoCapture(cv2.CAP_V4L2)
         if cap is None or not cap.isOpened():
             print('Warning: unable to open video source: ', i)
         else:
